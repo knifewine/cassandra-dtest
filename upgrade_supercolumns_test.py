e12c10dbccfa329593a9ea1ae29ed9e924baab71
Philip Thompson <ptnapoleon@gmail.com>
2014-12-22 10:03:18 -0500

    Remove logic for sc  upgrading if version is 2.1+

--- upgrade_supercolumns_test.py
+++ upgrade_supercolumns_test.py
@@ -37,13 +37,6 @@ class TestSCUpgrade(Tester):
         assert not cli.has_errors(), cli.errors()
         cli.close()
 
-        CASSANDRA_DIR = os.environ.get('CASSANDRA_DIR')
-        if get_version_from_build(CASSANDRA_DIR) >= '2.1':
-            #Upgrade nodes to 2.0.
-            #See CASSANDRA-7008
-            self.upgrade_to_version("git:cassandra-2.0")
-            time.sleep(.5)
-
         # Upgrade node 1
         node1.flush()
         time.sleep(.5)
@@ -90,25 +83,11 @@ class TestSCUpgrade(Tester):
         assert not cli.has_errors(), cli.errors()
         cli.close()
 
-
-        ##If we are on 2.1 or any higher version,
-        ##upgrade to 2.0.latest.
-        ##Otherwise, we must be on a 2.0.x, so we
-        ##should be upgrading to that version.
-        ##This will let us test upgrading
-        ##from 1.2.19 to each of the 2.0 minor releases.
-        CASSANDRA_DIR = os.environ.get('CASSANDRA_DIR')
-        if get_version_from_build(CASSANDRA_DIR) >= '2.1':
-            #Upgrade nodes to 2.0.
-            #See CASSANDRA-7008
-            self.upgrade_to_version("git:cassandra-2.0", [node1])
-            time.sleep(.5)
-        else:
-            node1.drain()
-            node1.watch_log_for("DRAINED")
-            node1.stop(wait_other_notice=False)
-            self.set_node_to_current_version(node1)
-            node1.start(wait_other_notice=True)
+        node1.drain()
+        node1.watch_log_for("DRAINED")
+        node1.stop(wait_other_notice=False)
+        self.set_node_to_current_version(node1)
+        node1.start(wait_other_notice=True)
 
         cli = node1.cli()
         cli.do("use test")
@@ -131,22 +110,16 @@ class TestSCUpgrade(Tester):
                 for k in xrange(50):
                     cli3.do("incr sc_test['Counter1']['sc%d']['c%d'] by 1" % (i, j))
 
-        if get_version_from_build(CASSANDRA_DIR) >= '2.1':
-            #Upgrade nodes to 2.0.
-            #See CASSANDRA-7008
-            self.upgrade_to_version("git:cassandra-2.0", [node2, node3])
-            time.sleep(.5)
-        else:
-            node2.drain()
-            node3.drain()
-            node2.watch_log_for("DRAINED")
-            node3.watch_log_for("DRAINED")
-            node2.stop(wait_other_notice=False)
-            node3.stop(wait_other_notice=False)
-            self.set_node_to_current_version(node2)
-            self.set_node_to_current_version(node3)
-            node2.start(wait_other_notice=True)
-            node3.start(wait_other_notice=True)
+        node2.drain()
+        node3.drain()
+        node2.watch_log_for("DRAINED")
+        node3.watch_log_for("DRAINED")
+        node2.stop(wait_other_notice=False)
+        node3.stop(wait_other_notice=False)
+        self.set_node_to_current_version(node2)
+        self.set_node_to_current_version(node3)
+        node2.start(wait_other_notice=True)
+        node3.start(wait_other_notice=True)
 
         cli = node1.cli()
         cli.do("use test")
@@ -165,31 +138,6 @@ class TestSCUpgrade(Tester):
 
         assert not cli.has_errors(), cli.errors()
         cli.close()
-
-    def upgrade_to_version(self, tag, nodes=None):
-        debug('Upgrading to ' + tag)
-        if nodes is None:
-            nodes = self.cluster.nodelist()
-
-        for node in nodes:
-            debug('Shutting down node: ' + node.name)
-            node.drain()
-            node.watch_log_for("DRAINED")
-            node.stop(wait_other_notice=False)
-
-        # Update Cassandra Directory
-        for node in nodes:
-            node.set_install_dir(version=tag)
-            debug("Set new cassandra dir for %s: %s" % (node.name, node.get_install_dir()))
-        self.cluster.set_install_dir(version=tag)
-
-        # Restart nodes on new version
-        for node in nodes:
-            debug('Starting %s on new version (%s)' % (node.name, tag))
-            # Setup log4j / logback again (necessary moving from 2.0 -> 2.1):
-            node.set_log_level("INFO")
-            node.start(wait_other_notice=True)
-            node.nodetool('upgradesstables -a')
 
 def assert_scs(cli, names):
     assert not cli.has_errors(), cli.errors()
